44b6311e992a2eb3958cb4539836a592802714c4
Sofia Paavola <paavola.sofia@gmail.com>
2019-11-08 10:58:29 +0000

    figured out the destinations and plane animation

--- src/js/main.ts
+++ src/js/main.ts
@@ -21,7 +21,6 @@ am4core.useTheme(am4themes_animated);
 
 var chart = am4core.create("chartdiv", am4maps.MapChart);
 
-
 // Set map definition
 chart.geodata = am4geodata_worldLow;
 
@@ -42,7 +41,7 @@ polygonSeries.useGeodata = true;
 // Configure series
 var polygonTemplate = polygonSeries.mapPolygons.template;
 polygonTemplate.tooltipText = "{name}";
-polygonTemplate.fill = am4core.color("#C38D9E");
+polygonTemplate.fill = am4core.color("#ffc800");
 polygonTemplate.stroke = am4core.color("#000033");
 polygonTemplate.strokeWidth = 0.5;
 polygonTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer;
@@ -62,69 +61,69 @@ chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#fffff
 var hs = polygonTemplate.states.create("hover");
 hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
 
-// let animation;
-// setTimeout(function(){
-//   animation = chart.animate({property:"deltaLongitude", to:100000}, 20000000);
-// }, 3000)
 
-chart.seriesContainer.events.on("down", function(){
- //animation.stop();
-
- // Add line bullets
-var cities = chart.series.push(new am4maps.MapImageSeries());
+let cities = chart.series.push(new am4maps.MapImageSeries());
 cities.mapImages.template.nonScaling = true;
 
-var city = cities.mapImages.template.createChild(am4core.Circle);
+let city = cities.mapImages.template.createChild(am4core.Circle);
 city.radius = 6;
 city.fill = chart.colors.getIndex(0).brighten(-0.2);
 city.strokeWidth = 2;
 city.stroke = am4core.color("#fff");
 
 function addCity(coords, title) {
-    var city = cities.mapImages.create();
+    let city = cities.mapImages.create();
     city.latitude = coords.latitude;
     city.longitude = coords.longitude;
     city.tooltipText = title;
     return city;
 }
 
-var paris = addCity({ "latitude": 48.8567, "longitude": 2.3510 }, "Paris");
-var toronto = addCity({ "latitude": 43.8163, "longitude": -79.4287 }, "Toronto");
-var la = addCity({ "latitude": 34.3, "longitude": -118.15 }, "Los Angeles");
-var havana = addCity({ "latitude": 23, "longitude": -82 }, "Havana");
+let helsinki = addCity({ "latitude": 60.1699, "longitude": 24.9384 }, "Helsinki");
+let beijing = addCity({ "latitude": 39.9042, "longitude": 116.4074 }, "Beijing");
+let dallas = addCity({ "latitude": 32.7767, "longitude": -96.7970 }, "Dallas");
+let munich = addCity({ "latitude": 48.1351, "longitude": 11.5820 }, "Munich");
+let singapore = addCity({ "latitude": 1.3521, "longitude": 103.8198 }, "Singapore");
+let madrid = addCity({ "latitude": 40.4168, "longitude": -3.7038 }, "Madrid");
+let budapest = addCity({ "latitude": 47.4979, "longitude": 19.0402 }, "Budapest");
+let london = addCity({ "latitude": 51.5074, "longitude": -0.1278 }, "London");
 
 // Add lines
-var lineSeries = chart.series.push(new am4maps.MapArcSeries());
-lineSeries.mapLines.template.line.strokeWidth = 2;
+let lineSeries = chart.series.push(new am4maps.MapArcSeries());
+lineSeries.mapLines.template.line.strokeWidth = 4;
 lineSeries.mapLines.template.line.strokeOpacity = 0.5;
 lineSeries.mapLines.template.line.stroke = city.fill;
 lineSeries.mapLines.template.line.nonScalingStroke = true;
 lineSeries.mapLines.template.line.strokeDasharray = "1,1";
 lineSeries.zIndex = 10;
 
-var shadowLineSeries = chart.series.push(new am4maps.MapLineSeries());
+let shadowLineSeries = chart.series.push(new am4maps.MapLineSeries());
 shadowLineSeries.mapLines.template.line.strokeOpacity = 0;
 shadowLineSeries.mapLines.template.line.nonScalingStroke = true;
 shadowLineSeries.mapLines.template.shortestDistance = false;
 shadowLineSeries.zIndex = 5;
 
 function addLine(from, to) {
-    var line = lineSeries.mapLines.create();
+    let line = lineSeries.mapLines.create();
     line.imagesToConnect = [from, to];
     line.line.controlPointDistance = -0.3;
 
-    var shadowLine = shadowLineSeries.mapLines.create();
+    let shadowLine = shadowLineSeries.mapLines.create();
     shadowLine.imagesToConnect = [from, to];
 
     return line;
 }
 
-addLine(paris, toronto);
-addLine(toronto, la);
-addLine(la, havana);
+addLine(helsinki, beijing);
+addLine(beijing, dallas);
+addLine(dallas, munich);
+addLine(munich, singapore);
+addLine(singapore, madrid);
+addLine(madrid, budapest);
+addLine(budapest, london);
 
 // Add plane
-var plane = lineSeries.mapLines.getIndex(0).lineObjects.create();
+let plane = lineSeries.mapLines.getIndex(0).lineObjects.create();
 plane.position = 0;
 plane.width = 48;
 plane.height = 48;
@@ -133,20 +132,20 @@ plane.adapter.add("scale", function(scale, target) {
     return 0.5 * (1 - (Math.abs(0.5 - target.position)));
 })
 
-var planeImage = plane.createChild(am4core.Sprite);
-planeImage.scale = 0.08;
+let planeImage = plane.createChild(am4core.Sprite);
+planeImage.scale = 0.20;
 planeImage.horizontalCenter = "middle";
 planeImage.verticalCenter = "middle";
 planeImage.path = "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47";
-planeImage.fill = chart.colors.getIndex(2).brighten(-0.2);
+planeImage.fill = chart.colors.getIndex(20).brighten(-0.2);
 planeImage.strokeOpacity = 0;
 
-var shadowPlane = shadowLineSeries.mapLines.getIndex(0).lineObjects.create();
+let shadowPlane = shadowLineSeries.mapLines.getIndex(0).lineObjects.create();
 shadowPlane.position = 0;
 shadowPlane.width = 48;
 shadowPlane.height = 48;
 
-var shadowPlaneImage = shadowPlane.createChild(am4core.Sprite);
+let shadowPlaneImage = shadowPlane.createChild(am4core.Sprite);
 shadowPlaneImage.scale = 0.05;
 shadowPlaneImage.horizontalCenter = "middle";
 shadowPlaneImage.verticalCenter = "middle";
@@ -160,8 +159,8 @@ shadowPlane.adapter.add("scale", function(scale, target) {
 })
 
 // Plane animation
-var currentLine = 0;
-var direction = 1;
+let currentLine = 0;
+let direction = 1;
 function flyPlane() {
 
     // Get current line to attach plane to
@@ -172,8 +171,8 @@ function flyPlane() {
     shadowPlaneImage.rotation = planeImage.rotation;
 
     // Set up animation
-    var from, to;
-    var numLines = lineSeries.mapLines.length;
+    let from, to;
+    let numLines = lineSeries.mapLines.length;
     if (direction == 1) {
         from = 0
         to = 1;
@@ -192,13 +191,18 @@ function flyPlane() {
     }
 
     // Start the animation
-    var animation = plane.animate({
+    let animation = plane.animate({
         from: from,
         to: to,
         property: "position"
     }, 5000, am4core.ease.sinInOut);
     animation.events.on("animationended", flyPlane)
-    
+    /*animation.events.on("animationprogress", function(ev) {
+      let progress = Math.abs(ev.progress - 0.5);
+      //console.log(progress);
+      //planeImage.scale += 0.2;
+    });*/
+
     shadowPlane.animate({
         from: from,
         to: to,
@@ -220,9 +224,3 @@ function flyPlane() {
 
 // Go!
 flyPlane();
-
-}); //
-
-
-
-
